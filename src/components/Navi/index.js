33f3aaf88ca9a0ecfecd127bfc57696c07edbdcf
William Capozzoli <thinkocapo@gmail.com>
2019-01-28 13:33:39 -0500

    nav

--- src/components/Navi/index.js
+++ src/components/Navi/index.js
@@ -6,30 +6,29 @@ class Navi extends React.Component {
   render() {
     const { location, title } = this.props
     return (
-
-      <div class="fixed-top container-fluid nav-style">
-        <div class="row text-center">
-          <div class="col">
-            Home
-          </div>
-          <div class="col">
-            Brand
-          </div>
-          <div class="col">
-            {/* SITE_TITLE */}
-            {/* <div class="logo-size">SITE_TITLE</div> */}
-            <span class="logo-size">SITE_TITLE</span>
-          </div>
-          <div class="col">
-            Profile
-          </div>
-          <div class="col">
-            About
+      <nav>
+        <div class="fixed-top container-fluid nav-style">
+          <div class="row text-center">
+            <div class="col">
+              Home
+            </div>
+            <div class="col">
+              Brand
+            </div>
+            <div class="col">
+              {/* SITE_TITLE */}
+              {/* <div class="logo-size">SITE_TITLE</div> */}
+              <span class="logo-size">SITE_TITLE</span>
+            </div>
+            <div class="col">
+              Profile
+            </div>
+            <div class="col">
+              About
+            </div>
           </div>
         </div>
-    
-      </div>
-    
+      </nav>
     )
   }
 }

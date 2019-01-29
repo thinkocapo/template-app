e3ef8f49edd091ebbf3a2862bc4ee882b4ff822e
William Capozzoli <thinkocapo@gmail.com>
2019-01-29 15:26:22 -0500

    Pages 1 - 4 and Profile page-4 in

--- src/pages/page-4.js
+++ src/pages/page-4.js
@@ -2,25 +2,26 @@ import { graphql } from 'gatsby'
 import get from 'lodash/get'
 import Img from 'gatsby-image'
 import React from 'react'
-
 import { siteMetadata } from '../../gatsby-config'
 import Layout from 'components/Layout'
 import Meta from 'components/Meta'
 import Icon from 'components/Icon'
 
-class Page1 extends React.Component {
+class Page4 extends React.Component {
   render() {
     const { location, data } = this.props
 
     return (
       <Layout location={location}>
-        <Meta site={siteMetadata} title="Page1" />
+        <Meta site={siteMetadata} title="Page4" />
         <div className="body-container-parent">
 
           <section className="text-center bg-light">
             <div className="container">
-              <h1>Page 1</h1>
-              <p className="lead text-muted">Heading or Inspirational quote goes here</p>
+              <h1>Page 4</h1>
+              <p className="lead text-muted">Section Element wrapped in a div body-container-parent</p>
+              <div>
+              </div>
             </div>
           </section>
 
@@ -30,4 +31,4 @@ class Page1 extends React.Component {
   }
 }
 
-export default Page1
+export default Page4
